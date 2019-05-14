9141b9036170d9d1d1e9e165148f4e2ca24baa8d
Beto Dute <betodute@gmail.com>
2019-05-14 17:43:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -84,19 +84,22 @@ function moveDodger(e) {
 function moveDodgerLeft(DODGER) {
     window.requestAnimationFrame(function() {
     const left = positionToInteger(DODGER.style.left);
-    
-    if (left > 0) {
-      DODGER.style.left = `${left - 4}px`;
-    }
+      if (left > 0) {
+        DODGER.style.left = `${left - 4}px`;
+      }
     }
     );
 }
 
 function moveDodgerRight(DODGER) {
   window.requestAnimationFrame(function() {
-    const left = positionToInteger(DODGER.style.left)
+    const left = positionToInteger(DODGER.style.left);
     if (left < 360) {
        DODGER.style.left = `${left + 4}px`;
+    }
+  }
+  );
+}
 
 
 function positionToInteger(p) {
