64c55210e2f89e1fc5ba564ee4f843b6067a5e27
Beto Dute <betodute@gmail.com>
2019-05-14 17:39:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function moveDodgerLeft(DODGER) {
     window.requestAnimationFrame(function() {
     const left = positionToInteger(DODGER.style.left)
     if (left > 0) {
-    
+      DODGER.style.left = `${left - 4}px`;
     
 
      if (left > 0) {
