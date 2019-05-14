dbba91d653265bcb144bac09b8a16a8c69c97050
Beto Dute <betodute@gmail.com>
2019-05-14 17:35:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,6 +71,8 @@ function moveDodger(e) {
     const code = e.which
     if ([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
         e.preventDefault()
+        e.stopPropagation()
+        
     if (e.which === LEFT_ARROW) {
        e.preventDefault();
        e.stopPropagation();
